a7d748e237bba44f13bd7c9b2178890b22dad764
rahulYUV <rs6205903996@gmail.com>
2025-04-14 02:20:28 +0530

    number

---
+++ week-1/offline-class-1/level-1/NumberTwo.js
@@ -0,0 +1,26 @@
+// numebrs =>  the digits ;
+
+// let integer = 14;
+// let float = 3.14;
+// console.log(typeof(integer));
+// console.log(typeof(float));
+let newThing = Number(5);
+// console.log(newThing);
+
+let numb = "14.5fck14";
+// console.log(parseFloat(numb))
+
+let integer = 42; // Integer
+let float = 3.14; // Floating-point number
+
+
+let numFromString = Number("123");
+let noFromBool = Number(true);
+console.log(numFromString)
+console.log(noFromBool)
+// numebr methods
+
+// toFixed(digits)
+let num = 3.125555;
+    num = num.toFixed(2)
+console.log(num)
